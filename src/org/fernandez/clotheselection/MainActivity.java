31a14a8c550287068b4104ecc8dcc8121942bc91
Norman <ing.nfernandez@yahoo.com>
2014-06-26 20:58:16 -0400

    Initial Login -Initial login attempt implemented.

--- src/org/fernandez/clotheselection/MainActivity.java
+++ src/org/fernandez/clotheselection/MainActivity.java
@@ -1,65 +1,50 @@
 package org.fernandez.clotheselection;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+   private EditText  username=null;
+   private EditText  password=null;
+   private Button login;
+   int counter = 3;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      username = (EditText)findViewById(R.id.editText1);
+      password = (EditText)findViewById(R.id.editText2);
+      login = (Button)findViewById(R.id.button1);
+   }
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+   public void login(View view){
+      if(username.getText().toString().equalsIgnoreCase("fdolivares") && 
+      password.getText().toString().equals("felixdob92")){
+      Toast.makeText(getApplicationContext(), "Redirecting...", 
+      Toast.LENGTH_SHORT).show();
+   }	
+   else{
+      Toast.makeText(getApplicationContext(), "Wrong Credentials",
+      Toast.LENGTH_SHORT).show();
+      counter--;;
+      if(counter==0){
+         login.setEnabled(false);
+      }
 
+   }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+}
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
 
 }
